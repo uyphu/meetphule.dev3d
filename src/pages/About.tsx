73f95391658dce57441dd0e319d91bfd472c63f7
Phu Le <uyphu@yahoo.com>
2025-06-07 16:00:55 -0700

    [MPLD-1] first commit

---
+++ src/pages/About.tsx
@@ -0,0 +1,7 @@
+const About = () => (
+  <div className="text-center mt-10">
+    <h2 className="text-3xl font-semibold">About Me</h2>
+  </div>
+);
+
+export default About;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,7 @@
+const Home = () => (
+  <div className="text-center mt-10">
+    <h1 className="text-4xl font-bold">Welcome to MeetPhuLe 3D</h1>
+  </div>
+);
+
+export default Home;
